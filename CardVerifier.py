91c82a3da27bc6e51c3930fdb6113682e5ab97f1
Otavio Augusto <voiglet@gmail.com>
2022-07-23 19:07:57 -0300

    Create CardVerifier.py

---
+++ CardVerifier.py
@@ -0,0 +1,10 @@
+# 1 from the last digit we skip every other number and then sum them up
+# 2 from the last digit +1 we double them and then we sum them up
+# 3 sum up both results, if the final result == 0, then the card is valid
+
+
+# american express cards start with 34 or 37 and have 15 digits
+# mastercard cards start with 51-55 and have 16 digits
+# visa cards start with 4 and have 13 or 16 digits
+
+ 
