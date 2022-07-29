154343107968202d59770f3e37ce2f8f1fa68aaa
Otavio Augusto <voiglet@gmail.com>
2022-07-29 19:09:39 -0300

    Update CardVerifier.py

--- CardVerifier.py
+++ CardVerifier.py
@@ -8,16 +8,47 @@
 # visa cards start with 4 and have 13 or 16 digits
 
 
+
 cardNumberStr = str(input('Card Number: '))
 cardNumberInt = int(cardNumberStr)
 cardNumberLen = len(cardNumberStr)
 
-while cardNumberLen <= 0:
-    print(cardNumberInt)
-   
-  
-       
 
+firstCounter = cardNumberLen
+firstJubilu = 1
+firstSum = 0
+
+secondCounter = cardNumberLen-1
+secondJubilu = 2
+secondSum = 0
+
+
+while firstJubilu <= cardNumberLen:
+    currentNumber = (cardNumberInt // (10**(cardNumberLen-firstCounter)) % 10)
+    print((cardNumberInt // (10**(cardNumberLen-firstCounter)) % 10))
+    firstJubilu += 2
+    firstCounter -= 2
+    firstSum = firstSum + currentNumber
+   
+print (firstSum)
+
+while secondJubilu <= cardNumberLen:
+    currentNumberTwo = 2 * (cardNumberInt // (10**(cardNumberLen-secondCounter)) % 10)
+    if currentNumberTwo / 10 >= 1:
+        firstTerm = currentNumberTwo // 1 % 10
+        secondTerm = currentNumberTwo // 10 % 10
+        
+        finalTerm = firstTerm + secondTerm
+        secondSum = secondSum + finalTerm
+        
+    elif currentNumberTwo / 10 <= 1:
+         
+        print((cardNumberInt // (10**(cardNumberLen-secondCounter)) % 10))
+        secondJubilu += 2
+        secondCounter -= 2
+        secondSum = secondSum + currentNumber
+        
+print (secondSum)
 #for len(cardNumberStr):
 # 5502090966601067 
     
